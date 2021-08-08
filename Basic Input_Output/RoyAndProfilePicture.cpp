97b305aea1046299a969dbbae069de9acb3f2706
Abhay Tomar <75438874+abhay-tomar03@users.noreply.github.com>
2021-08-08 09:04:20 +0530

    Update RoyAndProfilePicture.cpp

--- Basic Input_Output/RoyAndProfilePicture.cpp
+++ Basic Input_Output/RoyAndProfilePicture.cpp
@@ -1,51 +1,27 @@
 #include<bits/stdc++.h>
-
-
-
-
 using namespace std;
 
 int main()
 
 {
-
     int l, n;
-
     cin>>l>>n;
-
     int w, h;
-
     for (int i=0; i<n; i++)
-
     {
-
         cin>>w>>h;
-
         if (w<l||h<l)
-
         {
-
             cout<<"UPLOAD ANOTHER"<<endl;
-
         }
-
         else if ( w==h)
-
         {
-
             cout<<"ACCEPTED"<<endl;
-
         }
-
         else
-
         {
-
             cout<<"CROP IT"<<endl;
-
         }
-
     }
-
 }
 
